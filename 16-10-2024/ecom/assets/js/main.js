a6816d8c5b34fc4f7806c49b21d296da5a2f65ba
Pradeep <pradeep@smartcow.ai>
2024-10-17 06:37:36 +0530

    15-10-2024 ecommerce practice website

---
+++ 16-10-2024/ecom/assets/js/main.js
@@ -0,0 +1,7 @@
+// Example: handle adding product to cart
+document.querySelectorAll('.btn-primary').forEach(btn => {
+    btn.addEventListener('click', () => {
+      alert('Product added to cart');
+    });
+  });
+  
